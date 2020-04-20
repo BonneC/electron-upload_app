0e4cfa7c7a2810c396f29a030c04a7e45bec704d
BonneC <bonnegr33n@gmail.com>
2020-04-20 14:16:05 +0200

    electron vue builder starter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BootstrapVue from 'bootstrap-vue';
+
 import router from './router'
 import store from './store'
 
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
